ae280e9715293a95a4f2a64465c729f4be019247
yaaamin <yaaamin0914@gmail.com>
2022-09-21 11:29:18 +0500

    exp

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,3 @@
-import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +6,22 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from './tailwind.css';
 
-export const meta: MetaFunction = () => ({
+export const meta = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links =()=>{
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ]
+}
 
 export default function App() {
   return (
